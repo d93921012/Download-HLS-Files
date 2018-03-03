4d1d66b02ce6248b1ae1088d31e9c4880e146593
ajax <ajaxchen@ntu.edu.tw>
2018-03-03 15:10:45 +0800

    判斷是否為 Variant Playlist

--- get_tshls.php
+++ get_tshls.php
@@ -1,24 +1,5 @@
 <?php
 /*
-Made by Kudusch (blog.kudusch.de, kudusch.de, @Kudusch)
-
----------
-
-DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-                    Version 2, December 2004
-
-Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
-Everyone is permitted to copy and distribute verbatim or modified
-copies of this license document, and changing it is allowed as long
-as the name is changed.
-
-DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
-
-0. You just DO WHAT THE FUCK YOU WANT TO.
-
----------
-
 # How to use
 
 - go to http://mediathek.daserste.de, open with iPad User Agent
@@ -29,6 +10,7 @@ TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
 - enjoy
 
 */
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 //runtime
@@ -37,48 +19,26 @@ $startTime = microtime(true);
 //get url from input
 $url = $argv[1];
 $root_url = getRootUrl($url);
-//get stream with highest bandwith
-$streamUrl = getHighBandwidthStream($url);
+$GLOBALS['root_url'] = $root_url;
+
+//get stream file with highest bandwith
+$streamFile = getStreamFile($url);
+
+if (streamHasChanged($streamFile)) {
+    echo "Playlist has been changed, delete cached files. \n";
+    shell_exec("rm -r files");
+}
+
 //get array of all links to *.ts files
-$list = getHlsFiles($root_url.'/'.$streamUrl);
+$playlist = getPlaylist($streamFile);
 
 //make new directory
 if (!is_dir('files')) {
     mkdir('files');
 }
 
-//download all files from array, name with 3 leading zeros
-//if file is longer than 166.5 minutes, adjust str_pad params
 echo "Download all files from array\n";
-//var_dump($list);
-
-$n = 1;
-foreach ($list as $key) {
-    $number = str_pad($n, 4, "0", STR_PAD_LEFT);
-    echo($n.": ".$key."\n");
-    $fn = "files/part.".$number.".ts";
-    if (file_exists($fn) && filesize($fn) > 0) {
-        echo "file exist, skip \n";
-    } else {
-        $tries = 0;
-        $tsf = @fopen($root_url.'/'.$key, 'r');
-        while ($tsf == false && $tries < 3) {
-            echo "Try again later ... \n";
-            sleep(3);
-            $tsf = @fopen($root_url.'/'.$key, 'r');
-            $tries++;
-        }
-        if (!$tsf) {
-            echo "Failed to download ts file\n";
-            exit;
-        }
-        file_put_contents('tmp.ts', $tsf);
-        rename('tmp.ts', $fn);
-        echo "download ok!\n";
-    }
-    $n++;
-}
-
+downloadSegmentFiles($playlist);
 //merge files and delte parts
 sleep(1);
 mergeFiles('files');
@@ -93,93 +53,99 @@ echo("\nRun in ".(microtime(true)-$startTime)." seconds.");
 function getRootUrl($masterUrl)
 {
     $arr = parse_url($masterUrl);
-//    var_dump($arr);
     $root_url = $arr['scheme'].'://'.$arr['host'];
-    var_dump($root_url);
+
+    $rpos = strrpos($arr['path'], '/');
+    $root_path = $root_url.substr($arr['path'], 0, $rpos);
+    $GLOBALS['root_path'] = $root_path;
+
     return $root_url;
 }
 
 /*
- * input: string
- * output: string
+ * Input: url string
+ * Output: content string
  */
-function getHighBandwidthStream($masterUrl) {
-    echo "Get content of master.m3u8\n";
-    $ch = curl_init($masterUrl);
+function getUrlFile($url)
+{
+    $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
-    $curl_res = curl_exec($ch);
+    $res = curl_exec($ch);
     curl_close($ch);
+    return $res;
+}
 
-    //return link to second last stream (https://developer.apple.com/library/ios/documentation/networkinginternet/conceptual/streamingmediaguide/FrequentlyAskedQuestions/FrequentlyAskedQuestions.html#//apple_ref/doc/uid/TP40008332-CH103-SW1)
-//    var_dump($curl_res);
-
+/*
+ * input: url string
+ * output: playlist string
+ */
+function getStreamFile($masterUrl)
+{
+    echo "Get high bandwidth stream file\n";
+    $curl_res = getUrlFile($masterUrl);
     $result = explode("#", $curl_res);
-//    var_dump($result);
-    $flg = false;
+
+    $isVariantPlaylist = false;
     for ($i = 0; $i < count($result); $i++) {
-//        array_shift($result);
         if (strpos($result[$i], 'EXT-X-STREAM-INF') !== false) {
-            $flg = true;
+            $isVariantPlaylist = true;
             break;
         }
     }
-//    var_dump($result);
 
-    if ($flg == false) {
-        echo "Data error, without STREAM-INF!\n";
-        exit;
+    // 若為 VariantPlaylist，取得第一個 playlist
+    if ($isVariantPlaylist == true) {
+        $result = explode("\n", $result[$i]);
+
+        if (isset($result[1])) {
+            $sUrl = $result[1];
+            if (strpos($sUrl, 'http') !== 0) {
+                $sUrl = $GLOBALS['root_url'].'/'.$sUrl;
+            }
+            $curl_res = getUrlFile($sUrl);
+        } else {
+            echo "Data error!\n";
+            exit;
+        }
     }
 
-    $result = explode("\n", $result[$i]);
-//    var_dump($result);
+    return $curl_res;
+}
 
-    if (!isset($result[1])) {
-        echo "Data error!\n";
-        exit;
+/*
+ * 判斷 playlist 是否已改變
+ */
+function streamHasChanged($streamFile)
+{
+    $flg = false;
+
+    if (!file_exists('playlist.m3u8')) {
+        $flg = true;
+    } else {
+        $chk1 = md5_file('playlist.m3u8');
+        $chk2 = md5($streamFile);
+        if ($chk1 != $chk2) {
+            $flg = true;
+        }
     }
-    $str = $result[1];
-    echo "HLS url: {$str} \n";
-    return $str;
+
+    if ($flg == true) {
+        file_put_contents('playlist.m3u8', $streamFile);
+    }
+
+    return $flg;
 }
 
 /*
  * input: string
  * output: array
  */
-function getHlsFiles($streamUrl) {
-    echo "Get content of *.m3u8 file \n";
-//    var_dump($streamUrl);
-    $ch = curl_init($streamUrl);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
-    $raw = curl_exec($ch);
-    curl_close($ch);
+function getPlaylist($plstr) {
+    echo "Get playlist \n";
 
-//    var_dump($raw);
-    //remove comments and unnecessary data
-    $list_raw = explode("\n", $raw);
+    $list_raw = explode("\n", $plstr);
 
-    /*
-    for ($i = 0; $i < 5; $i++) {
-        array_shift($list_raw);
-    }
-
-    for ($i = 0; $i < 2; $i++) {
-        array_pop($list_raw);
-    }
-
-    var_dump($list_raw);
-    //extract file links
-    $list = array();
-    $i = 1;
-    foreach ($list_raw as $key) {
-        if($i % 2 == 0) {
-            array_push($list, $key);
-        }
-        $i++;
-    }
-    */
     $list = [];
 
     while (count($list_raw) > 0) {
@@ -196,7 +162,49 @@ function getHlsFiles($streamUrl) {
     return $list;
 }
 
-function mergeFiles($dirName) {
+// download all files from array, name with 3 leading zeros
+// if file is longer than 166.5 minutes, adjust str_pad params
+// 已修改為加上 4 個 0
+function downloadSegmentFiles($list)
+{
+    $n = 1;
+    foreach ($list as $key) {
+        $number = str_pad($n, 4, "0", STR_PAD_LEFT);
+        echo($n.": ".$key."\n");
+        $fn = "files/part.".$number.".ts";
+        if (file_exists($fn) && filesize($fn) > 0) {
+            echo "file exist, skip \n";
+        } else {
+            $tries = 0;
+
+            if (strpos($key, '/') === 0) {
+                $key = $GLOBALS['root_url'].'/'.$key;
+            } else if (strpos($key, 'http') !== 0) {
+                $key = $GLOBALS['root_path'].'/'.$key;
+            }
+
+            $tsf = @fopen($key, 'r');
+            while ($tsf == false && $tries < 3) {
+                echo "Try again later ... \n";
+                sleep(3);
+                $tsf = @fopen($key, 'r');
+                $tries++;
+            }
+            if (!$tsf) {
+                echo "Failed to download ts file\n";
+                exit;
+            }
+            file_put_contents('tmp.ts', $tsf);
+            rename('tmp.ts', $fn);
+            echo "download ok!\n";
+        }
+        $n++;
+    }
+
+}
+
+function mergeFiles($dirName)
+{
     //get all *.ts files in directory
     $fileList = [];
 
@@ -210,16 +218,15 @@ function mergeFiles($dirName) {
     }
 
     asort($fileList);
-//    var_dump($fileList);
-//    exit;
 
-    shell_exec("rm movie.ts");
-    //join and remove parts
-//    echo "filelist: {$fileList}\n";
+    file_put_contents('movie.ts', '');
+
+    // join all parts
     foreach ($fileList as $ff) {
         $shellScript = "cat ".$ff." >> movie.ts";
         shell_exec($shellScript);
     }
 
+    shell_exec("ffmpeg -i movie.ts -c copy movie.mp4");
 //    shell_exec("rm -r files");
 }
