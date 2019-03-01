6c87d95a335240294ec7693f67bce7cd718daa85
ajax <ajaxchen@ntu.edu.tw>
2019-03-01 20:21:19 +0800

    Concurrent download

--- get_tshls.php
+++ get_tshls.php
@@ -71,6 +71,7 @@ function getUrlFile($url)
     $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 20);
     $res = curl_exec($ch);
     curl_close($ch);
     return $res;
@@ -169,44 +170,142 @@ function downloadSegmentFiles($list)
 {
     // 一邊下載，同時組合，可以一邊看
     file_put_contents('movie_tmp.ts', '');
+
+    // 平行下載的連線數
+    $parallel_no = 5;
     
     $n = 1;
-    foreach ($list as $key) {
-        $number = str_pad($n, 4, "0", STR_PAD_LEFT);
-        echo($n.": ".$key."\n");
-        $fn = "files/part.".$number.".ts";
-        if (file_exists($fn) && filesize($fn) > 0) {
-            echo "file exist, skip \n";
-        } else {
-            $tries = 0;
+    for ($ii=0; $ii < ceil(count($list)/$parallel_no); $ii++) {
 
-            if (strpos($key, '/') === 0) {
-                $key = $GLOBALS['root_url'].'/'.$key;
-            } else if (strpos($key, 'http') !== 0) {
-                $key = $GLOBALS['root_path'].'/'.$key;
-            }
+        $ts_lst = [];
 
-            $tsf = @fopen($key, 'r');
-            while ($tsf == false && $tries < 3) {
-                echo "Try again later ... \n";
-                sleep(3);
-                $tsf = @fopen($key, 'r');
-                $tries++;
+        for ($jj=0; $jj < $parallel_no; $jj++) {
+            $idx = $ii * $parallel_no + $jj;
+
+            if ($idx < count($list)) {
+                $key = $list[$idx];
+                $_n = $n+$jj;
+                $number = str_pad($_n, 5, "0", STR_PAD_LEFT);
+                echo($_n.": ".$key."\n");
+
+                $fn = "files/part_".$number.".ts";
+
+                if (strpos($key, '/') === 0) {
+                    $key = $GLOBALS['root_url'].'/'.$key;
+                } else if (strpos($key, 'http') !== 0) {
+                    $key = $GLOBALS['root_path'].'/'.$key;
+                }
+
+                $ts_lst[] = [$key, $fn];
             }
-            if (!$tsf) {
-                echo "Failed to download ts file\n";
-                exit;
-            }
-            file_put_contents('tmp.ts', $tsf);
-            rename('tmp.ts', $fn);
-            echo "download ok!\n";
         }
-        
-        $shellScript = "cat ".$fn." >> movie_tmp.ts";
-        shell_exec($shellScript);
-        $n++;
+
+
+        $tries = 0;
+
+        $tsf = curlGetFiles($ts_lst);
+        while ($tsf == false && $tries < 16) {
+            echo "Try again later ... \n";
+            sleep(3);
+            $tsf = curlGetFiles($ts_lst);
+            $tries++;
+        }
+        if (!$tsf) {
+            echo "Failed to download ts file\n";
+            exit;
+        }
+
+        // file_put_contents('tmp.ts', $tsf);
+        // rename('tmp.ts', $fn);
+        echo "download ok!\n";
+
+        foreach ($ts_lst as $ts) {
+            $fn = $ts[1];
+            $shellScript = "cat ".$fn." >> movie_tmp.ts";
+            shell_exec($shellScript);
+        }
+
+        $n += $parallel_no;
     }
 
+}
+
+function curlGetFiles(Array $ts_lst)
+{
+    $flg = true;
+
+    $ch_lst = [];
+    $mh = curl_multi_init();
+
+    foreach ($ts_lst as $par) {
+        $fn = $par[1];
+
+        if (file_exists($fn) && filesize($fn) > 0) {
+            echo "{$fn} file exist, skip \n";
+            continue;
+        }
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $par[0]);
+        curl_setopt($ch, CURLOPT_HEADER,0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT,10);
+
+        curl_multi_add_handle($mh, $ch);
+        $ch_lst[] = ['ch' => $ch, 'fn' => $fn];
+    }
+
+    $running = null;
+
+    // execute the handles
+    do {
+        $mrc = curl_multi_exec($mh, $running);
+    } while ($mrc == CURLM_CALL_MULTI_PERFORM);
+
+    while ($running && $mrc == CURLM_OK) {
+        if (curl_multi_select($mh) != -1) {
+            do {
+                $mrc = curl_multi_exec($mh, $running);
+            } while ($mrc == CURLM_CALL_MULTI_PERFORM);
+        }
+    }
+
+    // $info = curl_multi_info_read($mh);
+    // if (false !== $info) {
+    //     var_dump($info);
+    // }
+    // get content and remove handles
+
+    for ($i = 0; $i < count($ch_lst); $i++) {
+        $ch = $ch_lst[$i]['ch'];
+        $fn = $ch_lst[$i]['fn'];
+
+        if(curl_errno($ch)) {
+            echo "Download error: '{$fn}', error: " . curl_error($ch)."\n";
+            $flg = false;
+        } else {
+            $resultStatus = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+            if ($resultStatus == '200') {
+                $raw = curl_multi_getcontent($ch);
+                $fp = fopen($fn,'w');
+                fwrite($fp, $raw);
+                fclose($fp);
+            } else {
+                echo "Download error: '{$fn}', status: {$resultStatus}\n";
+                $flg = false;
+            }
+
+        }
+
+        // close the handle
+        curl_multi_remove_handle($mh, $ch);
+    }
+
+    curl_multi_close($mh);
+
+    return $flg;
 }
 
 function mergeFiles($dirName)
